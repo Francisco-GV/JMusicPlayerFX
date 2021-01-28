c4dc427700cd591457d7f4245bb018b5b8ad1a5e
Frank <francisco_gasca@hotmail.com>
2021-01-28 00:00:31 -0600

    Audio list now can be any list type

--- src/com/frank/jmusicplayerfx/media/PlayList.java
+++ src/com/frank/jmusicplayerfx/media/PlayList.java
@@ -1,28 +1,14 @@
 package com.frank.jmusicplayerfx.media;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class PlayList {
     private String name;
-    private final List<AudioFile> audioList;
+    private List<AudioFile> audioFiles;
 
     public PlayList(String name) {
         this.name = name;
-        audioList = new ArrayList<>();
-    }
-
-    public void add(AudioFile audioFile) {
-        audioList.add(audioFile);
-    }
-
-    public void addAll(AudioFile... files) {
-        audioList.addAll(Arrays.asList(files));
-    }
-
-    public void remove(AudioFile audioFile) {
-        audioList.remove(audioFile);
     }
 
     public void changeName(String name) {
@@ -33,7 +19,11 @@ public class PlayList {
         return name;
     }
 
-    public List<AudioFile> getAudioList() {
-        return audioList;
+    public void setAudioFiles(List<AudioFile> audioFiles) {
+        this.audioFiles = audioFiles;
+    }
+
+    public List<AudioFile> getAudioFiles() {
+        return audioFiles;
     }
 }
