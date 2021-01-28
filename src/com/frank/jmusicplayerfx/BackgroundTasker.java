f024b816e822ae3398cea7a3324704c59926174d
Frank <francisco_gasca@hotmail.com>
2021-01-27 23:56:54 -0600

    added a static global Timer to manage background timer tasks

--- src/com/frank/jmusicplayerfx/BackgroundTasker.java
+++ src/com/frank/jmusicplayerfx/BackgroundTasker.java
@@ -3,9 +3,17 @@ package com.frank.jmusicplayerfx;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BackgroundTasker {
-    public static void executeTask(Task<?> task, EventHandler<WorkerStateEvent> onSucceeded) {
+    public static final Timer timerTasker;
+
+    static {
+        timerTasker = new Timer(true);
+    }
+
+    public static void executeGUITaskOnce(Task<?> task, EventHandler<WorkerStateEvent> onSucceeded) {
         task.setOnSucceeded(onSucceeded);
         executeInOtherThread(task);
     }
@@ -14,5 +22,9 @@ public class BackgroundTasker {
         Thread backgroundThread = new Thread(runnable);
         backgroundThread.setDaemon(true);
         backgroundThread.start();
+    }
+
+    public static void executePeriodically(TimerTask task, long delay, long period) {
+        timerTasker.schedule(task, delay, period);
     }
 }
