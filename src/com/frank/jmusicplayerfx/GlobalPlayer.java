2b4f70e1fe07cd475efc7e7dfbc8b98c9302a097
Frank <francisco_gasca@hotmail.com>
2021-02-05 22:25:09 -0600

    Added repeat function

--- src/com/frank/jmusicplayerfx/GlobalPlayer.java
+++ src/com/frank/jmusicplayerfx/GlobalPlayer.java
@@ -1,51 +1,82 @@
 package com.frank.jmusicplayerfx;
 
 import com.frank.jmusicplayerfx.media.AudioFile;
-import com.frank.jmusicplayerfx.media.PlayList;
+import com.frank.jmusicplayerfx.media.Playlist;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import static com.frank.jmusicplayerfx.JMusicPlayerFX.getInstance;
 
 public final class GlobalPlayer {
-    private final ObjectProperty<MediaPlayer> mediaPlayerProperty;
-    private final ObjectProperty<AudioFile> currentAudioProperty;
-    private PlayList currentPlayList;
+    private final ObjectProperty<MediaPlayer> mediaPlayer;
+    private final ObjectProperty<AudioFile> currentAudio;
+    private final ObjectProperty<Repeat> repeat;
+    private final DoubleProperty volume;
+    private final BooleanProperty mute;
+    private final BooleanProperty shuffle;
+    private Playlist currentPlaylist;
+    // temporalPlaylist is use to save the ordered playlist when shuffle is used
     private int currentIndex;
 
-    public GlobalPlayer() {
-        mediaPlayerProperty = new ReadOnlyObjectWrapper<>();
-        currentAudioProperty = new ReadOnlyObjectWrapper<>();
+    public enum Repeat {
+        DISABLE,
+        PLAYLIST,
+        SONG
+    }
 
-        mediaPlayerProperty.addListener((obs, oldPlayer, newPlayer) -> {
-            double volume = 0.10;
+    public GlobalPlayer() {
+        mediaPlayer = new ReadOnlyObjectWrapper<>();
+        currentAudio = new ReadOnlyObjectWrapper<>();
+        volume = new SimpleDoubleProperty(0.5);
+        mute = new SimpleBooleanProperty(false);
+        repeat = new SimpleObjectProperty<>(Repeat.DISABLE);
+        shuffle = new SimpleBooleanProperty(false);
+
+        mediaPlayer.addListener((obs, oldPlayer, newPlayer) -> {
             if (oldPlayer != null) {
-                volume = oldPlayer.getVolume();
                 oldPlayer.stop();
             }
+            if (newPlayer != null) {
+                newPlayer.volumeProperty().bind(volumeProperty());
+                newPlayer.muteProperty().bind(muteProperty());
 
-            newPlayer.setVolume(volume);
-            newPlayer.setOnEndOfMedia(() -> {
-                next();
-                play();
-            });
+                newPlayer.setOnEndOfMedia(() -> {
+                    if (repeat.get() == Repeat.SONG) {
+                        newPlayer.seek(Duration.ZERO);
+                        play();
+                    } else if (hasNext()) {
+                        next();
+                        play();
+                    }
+                });
+            }
         });
 
-        currentAudioProperty.addListener((obs, oldAudio, newAudio) -> {
+        currentAudio.addListener((obs, oldAudio, newAudio) -> {
             if (newAudio != null) {
                 getInstance().setTitle(newAudio.toString());
             } else {
                 getInstance().resetTitle();
             }
         });
+
+        shuffle.addListener((obs, old, shuffle) -> {
+
+        });
     }
 
-    public void initNewAudio(PlayList playList, AudioFile audioFile) {
+    public void initNewAudio(Playlist playList, AudioFile audioFile) {
         int index = 0;
 
-        if (currentPlayList != playList) {
-            setCurrentPlayList(playList);
+        if (currentPlaylist != playList) {
+            setCurrentPlaylist(playList);
         }
         if (playList != null) {
             index = playList.indexOf(audioFile);
@@ -53,28 +84,32 @@ public final class GlobalPlayer {
 
         setCurrentIndex(index);
 
-        currentAudioProperty.set(audioFile);
-        mediaPlayerProperty.set(audioFile.getMediaPlayer());
+        currentAudio.set(audioFile);
+        mediaPlayer.set(audioFile.getMediaPlayer());
     }
 
     private void initNewAudio(int index) {
-        AudioFile audioFile = currentPlayList.get(index);
+        AudioFile audioFile = getCurrentPlaylist().get(index);
         setCurrentIndex(index);
 
-        currentAudioProperty.set(audioFile);
-        mediaPlayerProperty.set(audioFile.getMediaPlayer());
+        currentAudio.set(audioFile);
+        mediaPlayer.set(audioFile.getMediaPlayer());
     }
 
     public void play() {
-        mediaPlayerProperty.get().play();
+        mediaPlayer.get().play();
     }
 
     public void pause() {
-        mediaPlayerProperty.get().pause();
+        mediaPlayer.get().pause();
+    }
+
+    public void stop() {
+        mediaPlayer.get().stop();
     }
 
     public void previous() {
-        if (currentPlayList != null) {
+        if (currentPlaylist != null) {
             if (currentIndex > 0) {
                 currentIndex--;
                 initNewAudio(currentIndex);
@@ -83,20 +118,28 @@ public final class GlobalPlayer {
     }
 
     public void next() {
-        if (currentPlayList != null) {
-            if (currentIndex < currentPlayList.getAudioFiles().size() - 1) {
+        if (currentPlaylist != null) {
+            if (currentIndex < getCurrentPlaylist().getAudioFiles().size() - 1) {
                 currentIndex++;
+                initNewAudio(currentIndex);
+            } else if (repeat.get() == Repeat.PLAYLIST) {
+                currentIndex = 0;
                 initNewAudio(currentIndex);
             }
         }
     }
 
-    public void setCurrentPlayList(PlayList playList) {
-        this.currentPlayList = playList;
+    public boolean hasNext() {
+        if (currentPlaylist != null) {
+            if (currentIndex + 1 < getCurrentPlaylist().getAudioFiles().size()) {
+                return true;
+            } else return repeat.get() == Repeat.PLAYLIST;
+        }
+        return false;
     }
 
-    public void setVolume(double volume) {
-        mediaPlayerProperty.get().setVolume(volume);
+    public void setCurrentPlaylist(Playlist playlist) {
+        currentPlaylist = playlist;
     }
 
     public void setCurrentIndex(int currentIndex) {
@@ -104,14 +147,34 @@ public final class GlobalPlayer {
     }
 
     public ObjectProperty<AudioFile> currentAudioProperty() {
-        return currentAudioProperty;
+        return currentAudio;
     }
 
     public ObjectProperty<MediaPlayer> mediaPlayerProperty() {
-        return mediaPlayerProperty;
+        return mediaPlayer;
     }
 
-    public PlayList getCurrentPlayList() {
-        return currentPlayList;
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer.get();
+    }
+
+    public DoubleProperty volumeProperty() {
+        return volume;
+    }
+
+    public BooleanProperty muteProperty() {
+        return mute;
+    }
+
+    public ObjectProperty<Repeat> repeatProperty() {
+        return repeat;
+    }
+
+    public BooleanProperty shuffleProperty() {
+        return shuffle;
+    }
+
+    public Playlist getCurrentPlaylist() {
+        return currentPlaylist;
     }
 }
